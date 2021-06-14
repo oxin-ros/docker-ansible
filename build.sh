cacf6a54d43565091c0d52ea5c0d76e6c03d4ea4
yremmet <yremmet@users.noreply.github.com>
2021-06-14 11:12:02 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,12 @@
 #/bin/sh
 
-#for dir in $(ls -d */); do 
-#    tag=$(echo $dir | cut -d"/" -f1)
-#    cd $dir 
-#    docker build . -t $1/ansible:$tag
-#    docker push $1/ansible:$tag 
-#done
-
-tag=ubuntu20.04
-cd ubuntu20.04/ 
-docker build . -t $1/ansible:$tag
-docker push $1/ansible:$tag
+for dir in $(ls -d */); do 
+    tag=$(echo $dir | cut -d"/" -f1)
+    cd $dir 
+    docker build . -t $1/ansible:$tag
+    docker push $1/ansible:$tag 
+    cd ..
+done
 
 docker tag $1/ansible:ubuntu20.04 $1/ansible:latest
 docker tag $1/ansible:ubuntu20.04 $1/ansible:ubuntu20.04-1
