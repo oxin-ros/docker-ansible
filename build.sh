569c95b4e65a1df918174bf814be04f48183582d
David-Michael Buckman <dmbuck32@gmail.com>
2022-07-19 09:58:54 +1200

    cleanup build script.

--- build.sh
+++ build.sh
@@ -1,13 +1,11 @@
 #/bin/sh
 
-for dir in $(ls -d */); do 
+for dir in $(ls -d */); do
     tag=$(echo $dir | cut -d"/" -f1)
-    cd $dir 
-    docker build . -t $1/ansible:$tag
-    docker push $1/ansible:$tag 
+    cd $dir
+    docker build . -t ansible-playbook:$tag
+    docker push ansible-playbook:$tag
     cd ..
 done
 
-docker tag $1/ansible:ubuntu20.04-pip $1/ansible:latest
-docker push $1/ansible:latest
-
+docker tag ansible-playbook:alpine3 ansible-playbook:latest
